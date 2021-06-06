6c4ef49cbf0ecc48433e6563208234e8bcaa1924
nesibecetin <nesibe3cetin@hotmail.com>
2021-06-06 21:20:52 +0300

    refactoring

---
+++ hrms-backend/hrms/src/main/java/kodlama/hrms/entities/concretes/JobPosition.java
@@ -0,0 +1,36 @@
+package kodlama.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="job_positions")
+@Data
+public class JobPosition {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	public JobPosition() {
+		super();
+	}
+	
+	public JobPosition(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	
+}
+
