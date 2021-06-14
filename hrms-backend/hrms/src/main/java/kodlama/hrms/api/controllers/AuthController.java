9e3f359708ffa9f7a586c5111b39a51398d7ecf0
nesibecetin <nesibe3cetin@hotmail.com>
2021-06-14 20:23:27 +0300

    add CrossOrigin annotation

--- hrms-backend/hrms/src/main/java/kodlama/hrms/api/controllers/AuthController.java
+++ hrms-backend/hrms/src/main/java/kodlama/hrms/api/controllers/AuthController.java
@@ -2,6 +2,7 @@ package kodlama.hrms.api.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import kodlama.hrms.entities.dtos.EmployerRegisterDto;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin
 public class AuthController {
 	private AuthService authService;
 
